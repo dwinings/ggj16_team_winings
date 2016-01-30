0e1706495357ef6d2f5a2c77fcaf3fd163891044
Jacob-Laverty <jacob.laverty@gmail.com>
2016-01-30 13:26:22 -0500

    Can now move towers from the pallet.

---
+++ Assets/Scripts/TowerPallet.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class TowerPallet : MonoBehaviour
+{
+  public TOWER_TYPE tower_type;
+
+  private Vector3 startPosition;
+  private Vector2 relativePosition;
+
+  public enum TOWER_TYPE{TYPE1, TYPE2, TYPE3, TYPE4};
+
+  private Camera camera;
+  private static GameObject draggable;
+
+  public void Start() {
+    camera = Camera.main;
+  }
+
+  public void OnMouseDown() {
+    draggable = getDraggable ();
+  }
+
+  public void OnMouseDrag() {
+    Transform dt = draggable.transform;
+    relativePosition = camera.ScreenToWorldPoint (Input.mousePosition);
+    dt.position = relativePosition;
+  }
+
+  public void OnMouseUp() {
+    Destroy (draggable);
+  }
+
+  private GameObject getDraggable() {
+    if (draggable != null)
+      Destroy (draggable);
+    return Instantiate (gameObject);
+  }
+}
