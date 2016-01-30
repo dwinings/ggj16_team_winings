6c4e228c478f65b48f374ffa2b522056dc08103d
Ben R Harrison <benr.harrison@outlook.com>
2016-01-30 14:07:32 -0500

    added range limit to towers

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -3,14 +3,28 @@ using System.Collections;
 
 public class Tower : MonoBehaviour {
 
-  private Vector2 projectileSpawn;
+	private Vector2 projectileSpawn;
   public GameObject projectile;
+	public GameObject[] enemies;
+	public float range;
+
 	void Awake () {
     projectileSpawn = transform.position;
-    InvokeRepeating ("LaunchProjectile", 2, 1f);
+    InvokeRepeating ("InRange", 2, 1f);
+	}
+
+	void InRange() {
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		foreach (GameObject enemy in enemies) {
+			float distance = (Mathf.Sqrt(Mathf.Pow((transform.position.x - enemy.transform.position.x), 2) + Mathf.Pow((transform.position.y - enemy.transform.position.y), 2)));
+			if (distance > 0 && distance < range) {
+				LaunchProjectile ();
+			}
+		}
 	}
 
   void LaunchProjectile() {
     Instantiate (projectile, projectileSpawn, Quaternion.identity);
   }
+		
 }
