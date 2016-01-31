8cb2b7cf1032c3f9ed8d7b0d6980eb05c3fee103
Wesley Boynton <wes@boynton.io>
2016-01-30 21:13:02 -0500

    You gotta commit bruh! (Adding credits)

---
+++ Assets/Credits/Video/PlayVideo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class PlayVideo : MonoBehaviour {
+
+  public MovieTexture movie;
+  public AudioSource audio;
+
+	void Start () {
+    GetComponent<RawImage> ().texture = movie as MovieTexture;
+    audio = GetComponent<AudioSource> ();
+    audio.clip = movie.audioClip;
+    movie.Play ();
+    audio.Play ();
+	}
+	
+	void Update () {
+    if (!movie.isPlaying) {
+      Start ();
+      StartCoroutine(SomeCoroutine());
+    }
+	}
+
+  private IEnumerator SomeCoroutine()
+  {
+    yield return new WaitForSeconds (0);
+    Application.LoadLevel (0);
+  }
+}
